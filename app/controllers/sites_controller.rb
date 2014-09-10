b0cd3b5e03a32640330f5f8f6124fc49a971d5d7
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-09-09 21:48:52 -0700

    site#new setup and updated models to reflect associations

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,9 +1,28 @@
 class SitesController < ApplicationController
   def new
+  	@site = Site.new
   end
 
   def create
-  end
+		require 'open-uri'
+		url = params.require(:site)[:url]
+		site = Site.create(url: url)
+
+		contents = Nokogiri::HTML(open(site.url))
+		contents.css('a').each do |link|
+			link_href = link.attributes['href'].value
+			if (link_href.starts_with? '/')
+			    link_href = site.url + link_href
+			end
+
+			if (link_href.starts_with? 'http://', 'https://')
+			    response = Typhoeus.get(link_href)
+
+			    site.links.create(url: link_href, http_response: response.response_code)
+			end  
+	  end
+	  redirect_to site_path(site)
+	end
 
   def show
   end
